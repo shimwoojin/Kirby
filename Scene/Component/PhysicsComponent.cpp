ae5965afc852988e10d50ca520d698d3f53b9a0c
심우진 <ggoggal@gmail.com>
2023-02-07 15:06:54 +0900

    20230207_PhysicsComponent change

--- Scene/Component/PhysicsComponent.cpp
+++ Scene/Component/PhysicsComponent.cpp
@@ -3,6 +3,8 @@
 #include "TransformComponent.h"
 #include "Scene/Actors/Actor.h"
 
+#define MAX_GRAVITY 4
+
 void PhysicsComponent::Initialize()
 {
 }
@@ -27,6 +29,16 @@ void PhysicsComponent::Update()
 				{
 					is_on_ground = true;
 					gravity = 0.0f;
+
+					auto tile_position = scene_actor->GetTransform()->GetPosition();
+					auto tile_scale = scene_actor->GetTransform()->GetScale();
+					auto player_scale = actor->GetTransform()->GetScale();
+					auto setting_position_y = tile_position.y + (tile_scale.y / 2) + (player_scale.y /2);
+					if (position.y != setting_position_y)
+					{
+						position.y = setting_position_y;
+						transform->SetPosition(position);
+					}
 				}
 			}
 		}
@@ -35,7 +47,7 @@ void PhysicsComponent::Update()
 	if (is_on_ground == false)
 	{
 		gravity += 0.05f;
-		if (gravity > 4.0f) gravity = 4.0f;
+		if (gravity > MAX_GRAVITY) gravity = 4.0f;
 
 		position.y -= gravity;
 		transform->SetPosition(position);
@@ -64,7 +76,7 @@ bool PhysicsComponent::CheckOnGround(class Actor* map_tile)
 	int min_y2 = tile_position.y - tile_scale.y / 2;
 	int max_y2 = tile_position.y + tile_scale.y / 2;
 
-	if (min_x1 < max_x2 && max_x1 > min_x2 && min_y1 < max_y2 && max_y1 > max_y2) return true;
+	if (min_x1 < max_x2 && max_x1 > min_x2 && min_y1 <= max_y2 && min_y1 >= max_y2 - MAX_GRAVITY) return true;
 	else return false;
 }
 
