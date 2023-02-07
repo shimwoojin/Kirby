8840a8e5c1ccde0713421d4cedebe1f1de64685b
심우진 <ggoggal@gmail.com>
2023-02-07 16:50:17 +0900

    20230207

--- Utility/Collide/Collide.cpp
+++ Utility/Collide/Collide.cpp
@@ -46,3 +46,34 @@ bool Collide::CollideLeftRightDown(Actor* lhs, Actor* rhs)
 	if (min_x1 < max_x2 && max_x1 > min_x2 && max_y1 < max_y2 && max_y1 > min_y2) return true;
 	else return false;
 }
+
+uint Collide::GetCollide_Dir(Actor* lhs, Actor* rhs)
+{
+	D3DXVECTOR3 lhs_scale = lhs->GetTransform()->GetScale();
+	D3DXVECTOR3 lhs_position = lhs->GetTransform()->GetPosition();
+
+	D3DXVECTOR3 rhs_scale = rhs->GetTransform()->GetScale();
+	D3DXVECTOR3 rhs_position = rhs->GetTransform()->GetPosition();
+
+	int min_x1 = lhs_position.x - lhs_scale.x / 2;
+	int max_x1 = lhs_position.x + lhs_scale.x / 2;
+	int min_y1 = lhs_position.y - lhs_scale.y / 2;
+	int max_y1 = lhs_position.y + lhs_scale.y / 2;
+
+	int min_x2 = rhs_position.x - rhs_scale.x / 2;
+	int max_x2 = rhs_position.x + rhs_scale.x / 2;
+	int min_y2 = rhs_position.y - rhs_scale.y / 2;
+	int max_y2 = rhs_position.y + rhs_scale.y / 2;
+
+	Collide_Dir temp = Collide_Null;
+
+	if (min_x1 < max_x2 && max_x1 > min_x2 && min_y1 < max_y2 && max_y1 > min_y2)
+	{
+		if (min_x1 < max_x2) temp = (Collide_Dir)(temp | Collide_Left);
+		if (max_x1 > min_x2) temp = (Collide_Dir)(temp | Collide_Right);
+		if (min_y1 < max_y2) temp = (Collide_Dir)(temp | Collide_Down);
+		if (max_y1 > min_y2) temp = (Collide_Dir)(temp | Collide_Up);
+	}
+
+	return temp;
+}
--- Utility/Collide/Collide.h
+++ Utility/Collide/Collide.h
@@ -1,9 +1,19 @@
 #pragma once
 
+enum Collide_Dir : uint
+{
+	Collide_Null	= 0,
+	Collide_Left	= 1,
+	Collide_Right	= 2,
+	Collide_Up		= 4,
+	Collide_Down	= 8
+};
+
 class Collide
 {
 public:
 	static bool IsCollided(class Actor* lhs, class Actor* rhs);
 	static bool CollideLeftRightDown(class Actor* lhs, class Actor* rhs);
+	static uint GetCollide_Dir(class Actor* lhs, class Actor* rhs);
 };
 
